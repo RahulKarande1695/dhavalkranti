3f6310cad5cc76a57cbef08cae3eadd4f6e9717d
rahul <rahulkarande1695@gmail.com>
2024-05-30 16:37:01 +0530

    counter fixes

--- js/all.js
+++ js/all.js
@@ -188,8 +188,28 @@ TyperSetup();
 			speed : 0.15
 		});
 		
-    // 
-    $('[data-toggle="counter-up"]').counterUp({
-      delay: 5,
-      time: 2000
+
+    $(".testimonial-carousel-1").owlCarousel({
+      loop: true,
+      dots: false,
+      margin: 25,
+      autoplay: true,
+      slideTransition: 'linear',
+      autoplayTimeout: 0,
+      autoplaySpeed: 10000,
+      autoplayHoverPause: false,
+      responsive: {
+          0:{
+              items:1
+          },
+          575:{
+              items:1
+          },
+          767:{
+              items:2
+          },
+          991:{
+              items:3
+          }
+      }
   });
